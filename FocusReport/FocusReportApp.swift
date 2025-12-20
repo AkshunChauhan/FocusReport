d422bfcd8ea00bb5d7684e2af0ab6044495b6e56
Akshun <akshunchauhan099@gmail.com>
2025-12-19 17:32:33 -0700

    Initial Commit

---
+++ FocusReport/FocusReportApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocusReportApp.swift
+//  FocusReport
+//
+//  Created by Akshun Chauhan on 2025-12-19.
+//
+
+import SwiftUI
+
+@main
+struct FocusReportApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
